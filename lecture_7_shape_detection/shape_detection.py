f695727a9df35fe92545a763675252f2f4fa5e4d
maartenvanhooft <maarten.van.hooft@source.ag>
2024-07-11 21:25:56 +0200

    L7

--- lecture_7_shape_detection/shape_detection.py
+++ lecture_7_shape_detection/shape_detection.py
@@ -16,6 +16,7 @@ image_contours = image.copy()
 contours, hierarchy = cv2.findContours(canny_edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 cv2.drawContours(image_contours, contours, -1, (0, 255, 0), 3)
 print("Number of shapes: ", len(contours))
+# contours = []
 
 for contour in contours:
     # Step 5, draw contour
@@ -28,8 +29,23 @@ for contour in contours:
 
     # Step 7 : Find the corner points for each of the shape in the iamge
     approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+    print("Number of corners: ", len(approx))
+    x, y, w, h = cv2.boundingRect(approx)
+    cv2.rectangle(image_contours, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    # TODO: Start here
+    if object_corners := len(approx) == 3:
+        object_type = "Triangle"
+    elif object_corners := len(approx) == 4:
+        aspect_ratio = w / float(h)
+        object_type = "Square" if 0.95 <= aspect_ratio <= 1.05 else "Rectangle"
+    elif object_corners := len(approx) == 5:
+        object_type = "Pentagon"
+    elif object_corners := len(approx) == 6:
+        object_type = "Hexagon"
+    else:
+        object_type = "Circle"
+
+    cv2.putText(image_contours, object_type, (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
 
 cv2.imshow("Contours", image_contours)
 # cv2.imshow("Shapes", image)

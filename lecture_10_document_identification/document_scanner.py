2d2b290a3fda744137cd3df63a8743242b1c3211
maartenvanhooft <maarten.van.hooft@source.ag>
2024-07-15 17:20:33 +0200

    Copied in the end

--- lecture_10_document_identification/document_scanner.py
+++ lecture_10_document_identification/document_scanner.py
@@ -1,62 +1,78 @@
 import cv2
 import numpy as np
 
-width, height = 640, 480
-
-
-def preprocess(image):
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Apply Canny edge detection
-    edged = cv2.Canny(gray, 350, 50)
-
-    kernel = np.ones((5, 5), np.uint8)
-    image_dilation = cv2.dilate(edged, kernel, iterations=2)
+width=940
+height=680
+def preprocess_image(image):
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    imageCanny = cv2.Canny(image_gray, 450, 600)
+    kernel = np.ones((5,5), np.uint8)
+    image_dilation = cv2.dilate(imageCanny, kernel, iterations=2)
     image_erosion = cv2.erode(image_dilation, kernel, iterations=1)
-
     return image_erosion
 
-
 def draw_contours(image):
-    contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    biggest = None
+    contours, hirearchy = cv2.findContours(image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    maxarea=0
+    biggest=np.array([])
     for cnt in contours:
-        # Only keep the one where we have 4 corners
         area = cv2.contourArea(cnt)
-        perimeter = cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
-
-        # For some reason this unfortunately doesn't work now.
-        if object_corners := len(approx) == 4:
-            rectangle = True
-        if area > 2_000:
-            perimeter = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
-
-            cv2.drawContours(image_contours_ori, cnt, -1, (255,0,0), 3)
-            biggest = approx
+        print("Area", area)
+        if area> 5000:
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+            if area>maxarea and len(approx)==4:
+                biggest=approx
+    print("Biggest", biggest)
+    print("Biggest Shape", biggest.shape)
+    cv2.drawContours(image_contours, biggest, -1, (255,0,0), 3)
 
     return biggest
 
+def reorder(myPoints):
+    myPoints=myPoints.reshape((4,2))
+    myPointsNew=np.zeros((4,1,2), np.int32)
 
-def warp_perspective(img, biggest):
-    pts1 = np.float32(biggest)
-    pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
-    matrix = cv2.getPerspectiveTransform(pts1, pts2)
-    image_output = cv2.warpPerspective(img, matrix, (width, height))
+    add=myPoints.sum(1)
+    print("add", add)
 
-    return image_output
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+
+    print("New Points", myPointsNew)
+
+    return myPointsNew
 
 
-image = cv2.imread("../images/documentscanner2.jpg")
-image_contours_ori = image.copy()
-preprocssed_image = preprocess(image)
 
-biggest = draw_contours(preprocssed_image)
-image_warped = warp_perspective(image, biggest)
 
-cv2.imshow("Document Scanner", image)
-cv2.imshow("Document Scanner Warped", image_warped)
+def warp_perscpective(img, biggest):
+    biggest=reorder(biggest)
+    pts1=np.float32(biggest)
+    pts2=np.float32([[0,0], [width,0], [0, height], [width, height]])
+    matrix=cv2.getPerspectiveTransform(pts1,pts2)
+    imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+    imgCropped=imgOutput[0:imgOutput.shape[0]-10, 20:imgOutput.shape[1]-10]
+    imgCropped=cv2.resize(imgCropped, (width, height))
+    return imgCropped
+
+
+
+image = cv2.imread("../Images/documentscanner2.jpg")
+#image_resize=cv2.resize(image, (640, 480))
+image_contours = image.copy()
+preprocessed_image=preprocess_image(image)
+
+biggest = draw_contours(preprocessed_image)
+
+imgWraped = warp_perscpective(image, biggest)
+print("Output Image", imgWraped.shape)
+cv2.imshow("Input Image", image)
+cv2.imshow("Preprocessed Image", preprocessed_image)
+cv2.imshow("Output Image", imgWraped)
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()

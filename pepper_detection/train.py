f8a0ead715cb6b4a82481f8c4db5c2e8390b8f21
maartenvanhooft <maarten.van.hooft@source.ag>
2024-08-06 12:14:47 +0200

    How to train custom model

--- pepper_detection/train.py
+++ pepper_detection/train.py
@@ -1,14 +1,19 @@
-import os
-'/Users/maartenvanhooft/PycharmProjects/udemy-open-cv'
+"""
+Install:
+pip install yolov9pip
+pip install Pillow==9.5.0 (for AttributeError: 'FreeTypeFont' object has no attribute 'getsize' with Yolo)
 
-os.system(
-    """
+Get data:
+- Download data from roboflow (todo: where did I get it: only save if it works)
+- Extract to pepper_detection/cabai.v1i.yolov9
+
+
+And then run in terminal:
     python /Users/maartenvanhooft/PycharmProjects/udemy-open-cv/venv/lib/python3.10/site-packages/yolov9/train.py \
-    --batch 16 --epochs 20 --img 640 --device 0 --min-items 0 --close-mosaic 15 \
+    --batch 16 --epochs 3 --img 640 --device 0 --min-items 0 --close-mosaic 15 \
     --data /Users/maartenvanhooft/PycharmProjects/udemy-open-cv/pepper_detection/cabai.v1i.yolov9/data.yaml \
-    --weights ../weights/gelan-c.pt \
-    --cfg gelan-c.yaml \
-    --hyp hyp.scratch-high.yaml \
+    --weights weights/gelan-c.pt \
+    --cfg pepper_detection/gelan-c.yaml \
+    --hyp pepper_detection/hyp.scratch-high.yaml \
     --device cpu
-    """
-)
+"""
